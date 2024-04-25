9cbd1dbabcb8bb877ebe0ac73137a6aefe16be0e
Naimish <naimishbhesaniya212@gmail.com>
2024-04-26 00:37:29 +0530

    add faveiconsdsd

--- app/layout.js
+++ app/layout.js
@@ -46,7 +46,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           src="https://www.googletagmanager.com/ns.html?id=GTM-TKHCZ893"
           height="0"
           width="0"
-          style="display:none;visibility:hidden"
+          className="hidden"
+          // style="display:none;visibility:hidden"
         ></iframe>
       </noscript>
 
