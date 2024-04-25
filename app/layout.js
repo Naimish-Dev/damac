9485b2c680474e421bc4614d11262aa462edd5b0
Naimish <naimishbhesaniya212@gmail.com>
2024-04-26 00:35:22 +0530

    add faveiconsdsd

--- app/layout.js
+++ app/layout.js
@@ -15,21 +15,22 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <link rel="icon" href="favicon.ico" />
-      </Head>
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=AW-16542161149"
-      ></script>
-
-      <Head>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-16542161149'); `,
+function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TKHCZ893') `,
           }}
         />
-      </Head>
-      <Head>
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16542161149"
+        ></script>
+
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -40,6 +41,15 @@ window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(argume
       </Head>
       <script></script>
       <body className={inter.className}>{children}</body>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-TKHCZ893"
+          height="0"
+          width="0"
+          style="display:none;visibility:hidden"
+        ></iframe>
+      </noscript>
+
       <Conact />
     </html>
   );
