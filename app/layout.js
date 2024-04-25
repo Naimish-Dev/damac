3e9d1616769904255d3ab4022eb34fc43516d4b4
Naimish <naimishbhesaniya212@gmail.com>
2024-04-26 00:48:17 +0530

    new goolge

--- app/layout.js
+++ app/layout.js
@@ -15,40 +15,32 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <link rel="icon" href="favicon.ico" />
+
+     
         <script
           dangerouslySetInnerHTML={{
             __html: `
-function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TKHCZ893') `,
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TKHCZ893');
+              `,
           }}
         />
-
-        {/* <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-16542161149"
-        ></script>
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-             function gtag_report_conversion(url) { var callback = function () { if (typeof(url) != 'undefined') { window.location = url; } }; gtag('event', 'conversion', { 'send_to': 'AW-16542161149/W3aeCLuo8KkZEP2x9c89', 'value': 1.0, 'currency': 'AED', 'event_callback': callback }); return false; }
-            `,
-          }}
-        /> */}
       </Head>
-      <script></script>
-      <body className={inter.className}>{children}</body>
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-TKHCZ893"
-          height="0"
-          width="0"
-          className="hidden"
-        ></iframe>
-      </noscript>
+
+      <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TKHCZ893"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {children}
+      </body>
 
       <Conact />
     </html>
